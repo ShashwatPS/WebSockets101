41eb390038b580614d424456db1253448e519f0f
Shashwat Singh <shashwat123student@gmail.com>
2024-04-12 11:50:01 +0530

    Was testing something

--- node/src/index.ts
+++ node/src/index.ts
@@ -11,13 +11,14 @@ const wss = new WebSocketServer({ server });
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
-    // ws.on('message', function message(data, isBinary) {
-    //     wss.clients.forEach(function each(client) {
-    //         if (client.readyState === WebSocket.OPEN) {
-    //             client.send(data, { binary: isBinary });client
-    //         }
-    //     });
-    // });
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                console.log(data)
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
 
     ws.send('Hello! Message From Server!!');
 });
